b578496ae69128aa516b031239ffa981e0ba6d68
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-04-14 00:32:23 +0600

    Task 1 multiply
    Сделал функционал умножения

--- homework69_task1/urls.py
+++ homework69_task1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from homework69_task1.views import add, get_token_view, subtract
+from homework69_task1.views import add, get_token_view, subtract, multiply
 
 urlpatterns = [
     path('add/', add, name='add'),
     path('token/', get_token_view, name='token'),
     path('subtract/', subtract, name='subtract'),
+    path('multiply/', multiply, name='multiply'),
 ]
