072ec588f7cb19b5007c1d2a5b2b8d684d01a4f4
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-04-14 12:51:03 +0600

    Task 2
    Обработка ошибок

--- homework69_task1/views.py
+++ homework69_task1/views.py
@@ -1,49 +1,63 @@
 import json
 
-from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse, HttpResponseNotAllowed
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import HttpResponseBadRequest, JsonResponse
 
 
 # Create your views here.
-def get_data(request):
+def add(request):
     if request.body:
         content = json.loads(request.body)
         a = content.get('A')
         b = content.get('B')
-        if a is None or b is None:
-            return HttpResponseBadRequest('Missing A or B')
         try:
             a = int(a)
             b = int(b)
         except ValueError:
             return HttpResponseBadRequest('A or B is not a number')
-        return a, b
-    else:
-        return HttpResponseBadRequest('Missing Data')
-
-
-def add(request):
-    numbers = get_data(request)
-    result = numbers[0] + numbers[1]
+    result = a + b
     return JsonResponse({'answer': result})
 
 
 def subtract(request):
-    numbers = get_data(request)
-    result = numbers[0] - numbers[1]
+    if request.body:
+        content = json.loads(request.body)
+        a = content.get('A')
+        b = content.get('B')
+        try:
+            a = int(a)
+            b = int(b)
+        except ValueError:
+            return HttpResponseBadRequest('A or B is not a number')
+    result = a - b
     return JsonResponse({'answer': result})
 
 
 def multiply(request):
-    numbers = get_data(request)
-    result = numbers[0] * numbers[1]
+    if request.body:
+        content = json.loads(request.body)
+        a = content.get('A')
+        b = content.get('B')
+        try:
+            a = int(a)
+            b = int(b)
+        except ValueError:
+            return HttpResponseBadRequest('A or B is not a number')
+    result = a * b
     return JsonResponse({'answer': result})
 
 
 def divide(request):
-    numbers = get_data(request)
-    if numbers[1] == 0:
-        return HttpResponseBadRequest('Fatal Error. Division by zero')
+    if request.body:
+        content = json.loads(request.body)
+        a = content.get('A')
+        b = content.get('B')
+        try:
+            a = int(a)
+            b = int(b)
+        except ValueError:
+            return HttpResponseBadRequest('A or B is not a number')
+    if b == 0:
+        return HttpResponseBadRequest('Fatal error. Division by zero')
     else:
-        result = numbers[0] / numbers[1]
+        result = a / b
         return JsonResponse({'answer': result})
